2b1b62120f35445419126ec65cb46a1aea7a276f
Muhammad Rubiyanto Permana <rubiyanto.permana@gmail.com>
2017-05-31 08:31:02 +0700

    Initial Commit
    Initial Commit

---
+++ src/main/java/polban/jtk/absensimahasiswa/rombonganbelajar/RombonganbelajarMicroservicesServerApplication.java
@@ -0,0 +1,12 @@
+package polban.jtk.absensimahasiswa.rombonganbelajar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RombonganbelajarMicroservicesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RombonganbelajarMicroservicesServerApplication.class, args);
+	}
+}
