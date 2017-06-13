cf451694f05535187505527c43c18f3cf20ed04f
triayudap <triayuda.p@gmail.com>
2017-06-13 10:15:54 +0700

    menambahkan modul query getmhsbykelas

--- src/main/java/polban/jtk/absensimahasiswa/rombonganbelajar/MahasiswaController.java
+++ src/main/java/polban/jtk/absensimahasiswa/rombonganbelajar/MahasiswaController.java
@@ -1,18 +1,26 @@
 package polban.jtk.absensimahasiswa.rombonganbelajar;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/mahasiswa")
 public class MahasiswaController {
 	protected Logger logger = Logger.getLogger(MahasiswaController.class.getName());
+	
+	private static SessionFactory sessionFactory;
+	private static RestTemplate restTemplate;
 	
 	@Autowired    
 	MahasiswaRepository repository;
@@ -29,7 +37,7 @@ public class MahasiswaController {
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Mahasiswa ById(@RequestParam("nim") long id)
+	public Mahasiswa ById(@PathVariable("nim") long id)
 	{
 		//logger.info("kelas-microservice byId() invoked: " + id);
 		//Kelas kelas = KelasRepository.getKelas(id);
@@ -38,6 +46,24 @@ public class MahasiswaController {
 		Mahasiswa mhs = repository.findOne(nim);
 		return mhs;
 	}
+	
+	@RequestMapping(value = "/getmhsbykelas/{id}", method = RequestMethod.GET)
+	public List<String> mhsByKelas(@PathVariable("id_kelas") long id){
+		
+		
+			List<String> nim = new ArrayList<String>();
+			nim = this.sessionFactory.getCurrentSession().createQuery("SELECT nim  FROM Mahasiswa WHERE id_kelas in(:id_kelas)").setParameter("id_kelas", id).list();
+			List<String> mhs = new ArrayList<String>();
+			
+			for (String nimmhs : nim ){
+				mhs.add(restTemplate.getForObject("http://localhost:2002/mahasiswa/"+nimmhs, String.class));
+			}
+			
+			return mhs;			
+		
+		
+	}
+	
 	
 	@RequestMapping(value ="/deletebyid/{id}", method = RequestMethod.DELETE)
 	public String deleteById(@RequestParam("nim") long id)
